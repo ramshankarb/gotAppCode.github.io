6d0665a0a5657d8dde0398a9c4a4bccf40feb96e
ramshankarb <4612sainik@gmail.com>
2018-08-20 02:16:17 +0530

    13thCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { GotHttpService } from '../app/got-http.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Game of Thrones';
-  constructor(){
+  public allCharacters ;
+  public allBooks;
+  public allHouses;
+  constructor(public _gotHttpService : GotHttpService){
     console.log("App constructor");
   }
+  ngOnInit(){
+    this.allCharacters = this._gotHttpService.getAllCharacters().subscribe(
+      data =>{
+        console.log(data);
+        this.allCharacters = data;
+      },
+      error =>{
+        console.log("Error Occured");
+        console.log(error.errorMessage)
+      }
+    )
+    
+    this.allBooks = this._gotHttpService.getAllBooks().subscribe(
+      data =>{
+        console.log(data);
+        this.allBooks = data;
+      },
+      error =>{
+        console.log("Error Occured");
+        console.log(error.errorMessage);
+      }
+    )
+    
+    this.allHouses = this._gotHttpService.getAllHouses().subscribe(
+      data =>{
+        console.log(data);
+        this.allHouses = data;
+      },
+      error =>{
+        console.log("Error Occured");
+        console.log(error.errorMessage);
+      }
+    )
+}
 }
